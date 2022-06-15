aa1cbf2a58f88bc86ff9fc9575b609ad8f6d8bed
Jair Partida <jairpartida1@gmail.com>
2022-06-14 20:58:59 -0600

    files and folder structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {Box} from '@mui/material'
+import './index.css';
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Box width='400px' sx={{ width: { xl: '1488px' }}} m='auto'>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/exercise/:id' element={<ExerciseDetail />} />
+        </Routes>
+        <Footer />
+    </Box>
   );
 }
 
